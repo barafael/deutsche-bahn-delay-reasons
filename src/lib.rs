6320c9830ca010075b2d555271e0300d9744e353
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-07-18 11:51:41 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Deutsche Bahn is awesome, but often trains are late.
+//! There can be many reasons, some of which this library enumerates.
+//! Use the function [`get_grund`] to get a random delay reason.
+//!
+//! This library exists because I was waiting for a train.
+
 use thiserror::Error;
 
 use enum_index_derive::{IndexEnum};
@@ -5,49 +11,64 @@ use variant_count::VariantCount;
 
 mod random_grund;
 
+/// Get a random delay reason.
 pub fn get_grund() -> Grund {
     rand::random()
 }
 
+/// Some of the possible reasons a Deutsche Bahn train could be delayed.
 #[non_exhaustive]
 #[derive(Debug, Error, VariantCount, IndexEnum)]
 pub enum Grund {
-    #[error("Fahrt faellt aus")]
+    /// Trip cancelled.
+    #[error("Fahrt fällt aus")]
     FahrtFaelltAus,
 
+    /// Trip only reaches a certain stop today.
     #[error("Fährt heute nur bis {0}")]
     FaehrtHeuteNurBis(String),
 
+    /// Delays in operations.
     #[error("Verzögerungen im Betriebsablauf")]
     VerzoegerungenImBetriebsablauf,
 
+    /// Change of track.
     #[error("Gleiswechsel")]
     Gleiswechsel,
 
+    /// Trip cancelled, there is replacement trip.
     #[error("Fahrt fällt aus, es verkehrt Ersatzfahrt {0}")]
     FahrtFaelltAusMitErsatzfahrt(String),
 
+    /// Technical difficulties with the train.
     #[error("Technische Störungen am Zug")]
     TechnischeStoerungenAmZug,
 
+    /// Delay of a previous train.
     #[error("Verspätung eines vorausfahrenden Zuges")]
     VerspaetungEinesVorausfahrendenZuges,
 
+    /// Delayed allocation of the train.
     #[error("Verspätete Bereitstellung des Zuges")]
     VerspaeteteBereitstellungDesZuges,
 
+    /// Construction work.
     #[error("Bauarbeiten")]
     Bauarbeiten,
 
+    /// Weather-related difficulties.
     #[error("Witterungsbedingte Störung")]
     WitterungsbedingteStoerung,
 
+    /// Difficulties with a switch.
     #[error("Weichenstörung")]
     WeichenStoerung,
 
+    /// Changes in the journey course.
     #[error("Änderung im Fahrtverlauf")]
     AenderungImFahrtverlauf,
 
+    /// Storm or bad weather.
     #[error("Unwetter")]
     Unwetter,
 }
--- src/random_grund.rs
+++ src/random_grund.rs
@@ -4,9 +4,14 @@ use rand::Rng;
 
 use enum_index::IndexEnum;
 
+/// Implement a standard distribution for our central delay code enum.
+/// With this trait, `rand::random()` can be used to get a random enum variant.
+/// All members of enum variants must be default-constructible for that to work.
 impl Distribution<Grund> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Grund {
         let index = rng.gen_range(0..Grund::VARIANT_COUNT);
+
+        // unwrap here is OK by construction, given that the VARIANT_COUNT is derived correctly.
         Grund::index_enum(index).unwrap()
     }
 }
